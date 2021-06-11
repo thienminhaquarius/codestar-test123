71797a4c67312367f69c394d84b9754026113e2c
DESKTOP-NLNJOI0\TM <thienlm322@gmail.com>
2021-06-11 08:55:17 +0700

    [added] layer

--- src/hello-func/app.js
+++ src/hello-func/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var tuc = require('temp-units-conv');
 
 app.get('/', function(req, res) {
   res.send({
@@ -12,6 +13,12 @@ app.post('/', function(req, res) {
     "Output": "Hello World!"
   });
 });
+
+app.get('/temp', function(req, res) {
+  res.send({
+    'dependencies-test ': tuc.k2c(373.15)
+  })
+})
 
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
